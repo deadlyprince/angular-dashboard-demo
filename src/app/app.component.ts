10b859efc593464db09b66786d676f1f8fbe9413
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2017-11-28 13:13:31 +0530

    new client api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HtClientService} from "ht-angular";
+import { GetUrlParam } from "ht-utility";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor(
+    private clientService: HtClientService
+  ) {
+    const token = GetUrlParam('key');
+    if (token) this.clientService.token = token;
+  }
 }
